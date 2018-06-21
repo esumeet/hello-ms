86129e54f8faac5d003c3f9cd4b687c6794eb7cc
Sumeet <sumeetmondal@gmail.com>
2018-06-21 18:22:18 +0530

    Brahma

---
+++ src/main/java/com/iamsumeet/mstests/helloms/HelloMSApplication.java
@@ -0,0 +1,18 @@
+package com.iamsumeet.mstests.helloms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableWebMvc
+public class HelloMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMSApplication.class, args);
+	}
+}
